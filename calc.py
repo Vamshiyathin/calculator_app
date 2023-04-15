903f72b15ed3cd40789c545d402baa00527ebf2d
vamshi_pittala <mudhirajvamshi5@gmail.com>
2023-04-16 05:26:20 +0930

    adding new functionality of division for two numbers

---
+++ calc.py
@@ -0,0 +1,14 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+# This function divides two numbers
+def divide(x, y):
+    if y==0:
+        raise ValueError('cannot divide by 0')
+    return x / y
